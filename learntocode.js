3fbd08f3cdbbb132007b31bcc2a4319c82d78d79
Madhuri Patel <madhuri7patel@gmail.com>
2021-06-27 23:09:21 +0530

    intial commit

---
+++ learntocode.js
@@ -0,0 +1,111 @@
+	//var name ="madhuri";
+	//	var age ="22";
+		//var shootingScore = 45.6;
+
+	//	var message = "Hi, my name is " + name + " and I am" + age + "years old!"; 
+
+		//var FirstName = "Madhuri";
+		//var LastName = "Patel";
+		//var dateofBirth = "10-09-98";
+		//var age = "22";
+		//var profileImgUrl = "http://coolpicks.com/johnjacob.jpg";
+
+		//var loginWelcomeMessage= "Welcome,  " + FirstName +".Happy" + age + "rd birthday!" ;
+		//console.log(loginWelcomeMessage);
+
+		//var sum = 10+15;
+		//var sub = 15-7;
+		//var mul = 3*4;
+		//var div = 9/3;
+		//var mod = 10%3;
+
+		//var msg = "10/3= with a remainder of " + mod;
+
+//		var result = 10*50 +34-4;
+
+//		console.log(result);0
+
+	//var myAccountBalance = 300;
+	//var nikeSBshoes = 799.23;
+	//var coupon = 500;
+
+	//if (nikeSBshoes <= myAccountBalance) {
+	//	myAccountBalance -= nikeSBshoes;
+	//	console.log("i am a good person");
+	//	console.log("Accont Balance: " + myAccountBalance);
+	//} else if (nikeSBshoes - coupon <= myAccountBalance) {
+	//	console.log("i am a good person");
+	//	myAccountBalance -= nikeSBshoes - coupon;
+	//	console.log("Accont Balance: " + myAccountBalance);
+	//}
+	//else {
+	//	console.log("You too broke fo shoes bra!");
+	//}
+
+//	if (1===1 && 2===2 ){
+//		console.log("These are both true!");
+//	}
+
+//	if(true&&false){
+//		console.log("These are the same!");
+//	}
+
+//	var cat1 = 5;
+//	var cat2 = 1;
+//	var cat3 = 10;
+
+//	if(cat1>cat2 && cat1>cat3){
+//		console.log("Cat 1 is the cutest!");
+//	}
+
+	//var student1 = "Madhuri";
+	//var student2 = "Anoop";
+	//var student3 = "Arun";
+
+	//var students = ["Madhuri","Anoop","Arun"];
+
+	//var naughtyList = [];
+
+	//naughtyList.push(students[0]);
+
+	//var index = naughtyList.indexOf("Arun");
+	//console.log(naughtyList);
+
+	//if(index>-1){
+	//	naughtyList = naughtyList.splice(index, 1);
+	//}
+
+	//console.log(naughtyList);
+
+	//var index = 10;
+	//for(var x =0; x < index; x++){
+	//	console.log(x);
+	//}
+
+	//var students = ["john","jacob","jingle","smith"];
+
+	//for(var index = 0; index< students.length; index++){
+	//	console.log(students[index]);
+	//}
+
+	//console.log(students[0]);
+	//console.log(students[1]);
+
+	//var length1 = 15;
+	//var width1 = 10;
+	//var area = length1*width1;
+
+	//console.log(area);
+
+	function area (length,width){
+		return length*width;
+	}
+
+	var reactangleAreas =[]
+	reactangle.push(area (10,15));
+//	var area1 = area(10,15);
+	reactangleAreas.push(area(14,2));
+
+	console.log(reactangleAreas);
+
+	
